004ad175e24d0272441cdd645567bef90a5121bd
Victor Hugo Honorato <78669289+VhHonorato@users.noreply.github.com>
2021-10-27 11:18:00 -0300

    Add files via upload

---
+++ desafioFord.js
@@ -0,0 +1,34 @@
+let entrada = true;
+let saida = true;
+
+    const norte = {
+        entrada: false,
+        saida = true,
+    }
+
+    const sul = {
+        entrada: true,
+        saida: false
+    }
+
+    const nordeste = {
+        entrada: true,
+        saida: false
+    }
+
+    const centroOeste = {
+        entrada: true,
+        saida: false
+    }
+
+    const sudeste = {
+        entrada: false,
+        saida: true
+    }
+
+    const sul = {
+        entrada: true,
+        saida: false
+    }
+
+    if(norte.entrada == false)
